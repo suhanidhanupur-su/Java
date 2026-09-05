94b676256f4817eaa37e74cd68b8519b89895c35
Suhani kumari <suhanidhanupur@gmail.com>
2026-09-05 17:29:27 +0530

    Added

---
+++ Firstclass.java
@@ -0,0 +1,11 @@
+// class Firstclass{
+//     public static void main(String args[]) {
+//         System.out.println("Hello world");
+//     }
+// }
+class  Main{
+    public static void main(String args[] ){
+        System.out.println("Hello ,I'm Suhani.");
+        System.out.println("I am learning Java.");
+    }
+}
